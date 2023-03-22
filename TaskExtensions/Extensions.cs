838138a8792012417f1ff6ebce0ec5859650d1af
murat-boz <muratboz@hotmail.com>
2023-03-22 22:43:31 +0300

    Add project files.

---
+++ TaskExtensions/Extensions.cs
@@ -0,0 +1,49 @@
+﻿namespace TaskExtensions
+{
+    public static class Extensions
+    {
+        public static async Task<T> WithCancellation<T>(this Task<T> task, CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<object>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+            // This disposes the registration as soon as one of the tasks trigger
+            using (cancellationToken.Register(state =>
+            {
+                ((TaskCompletionSource<object>)state).TrySetResult(null);
+            },
+            tcs))
+            {
+                var resultTask = await Task.WhenAny(task, tcs.Task);
+                if (resultTask == tcs.Task)
+                {
+                    // Operation cancelled
+                    throw new OperationCanceledException(cancellationToken);
+                }
+
+                return await task;
+            }
+        }
+
+        public static async Task<T> TimeoutAfter<T>(this Task<T> task, TimeSpan timeout)
+        {
+            using (var cts = new CancellationTokenSource())
+            {
+                var delayTask = Task.Delay(timeout, cts.Token);
+
+                var resultTask = await Task.WhenAny(task, delayTask);
+                if (resultTask == delayTask)
+                {
+                    // Operation cancelled
+                    throw new OperationCanceledException();
+                }
+                else
+                {
+                    // Cancel the timer task so that it does not fire
+                    cts.Cancel();
+                }
+
+                return await task;
+            }
+        }
+    }
+}
